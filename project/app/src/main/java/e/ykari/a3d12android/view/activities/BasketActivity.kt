97162fe8e8e3d48436e3d0a40c338c554ee492aa
Ynnkrim <ykarim@hotmail.fr>
2018-11-19 09:20:06 +0100

    19/11/2018

--- project/app/src/main/java/e/ykari/a3d12android/view/activities/BasketActivity.kt
+++ project/app/src/main/java/e/ykari/a3d12android/view/activities/BasketActivity.kt
@@ -1,8 +1,14 @@
 package e.ykari.a3d12android.view.activities
 
+import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import e.ykari.a3d12android.R
 
 class BasketActivity : AppCompatActivity() {
 
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_basket)
 
+    }
 }
